fa3154a695fba18ef65b589a9e6cbb6949cfcab3
Sergio del Amo <sergio.delamo@softamo.com>
2025-11-05 12:03:35 +0100

    Update neo4j 6.0.2 (#597)
    * Update to Neo4j 6.0.2
    
    https://neo4j.com/docs/java-manual/current/upgrade
    
    > Session — Methods .asyncSession() and .reactiveSession() have been removed. Use Session(Class), with Class controlling the desired type of session.
    
    > Session — Methods .readTransaction() and .writeTransaction() have been removed in favor of .executeRead() and .executeWrite(). The same holds for their async conterparts .executeReadAsync() and .executeWriteAsync(). The new methods behave similarly, but the argument types are different and the new methods do not support the .commit(), .rollback(), .close(), and .closed() methods
    
    * Remove deprecated EmbeddedNeo4jServer and managed-neo4j-harness for major version
    
    https://github.com/micronaut-projects/micronaut-neo4j/pull/422
    
    * micronaut-test = "4.10.2"
    
    * micronaut-validation = "4.12.0"

--- neo4j-bolt/src/test/groovy/io/micronaut/neo4j/bolt/Neo4jServerSpec.groovy
+++ neo4j-bolt/src/test/groovy/io/micronaut/neo4j/bolt/Neo4jServerSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2023 original authors
+ * Copyright 2017-2024 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,50 +26,7 @@ import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 
-/**
- * @author graemerocher
- * @since 1.0
- */
-class Neo4jEmbeddedServerSpec extends Specification {
-
-    @Shared
-    int EMBEDDED_NEO4J_TCP_PORT = SocketUtils.findAvailableTcpPort()
-
-    @Ignore
-    void "test neo4j embedded"() {
-        given:
-        ApplicationContext applicationContext = ApplicationContext.run(
-                'neo4j.uri':"bolt://localhost:${EMBEDDED_NEO4J_TCP_PORT}",
-                'neo4j.embedded.ephemeral':true
-        )
-
-        when:
-        Driver driver = applicationContext.getBean(Driver)
-
-        then:
-        driver.session().run('MATCH (n) RETURN n').size() == 0
-
-        cleanup:
-        applicationContext?.stop()
-    }
-
-    @Ignore
-    void "test neo4j embedded again on the same port"() {
-        given:
-        ApplicationContext applicationContext = ApplicationContext.run(
-            'neo4j.uri':"bolt://localhost:${EMBEDDED_NEO4J_TCP_PORT}",
-            'neo4j.embedded.ephemeral':true
-        )
-
-        when:
-        Driver driver = applicationContext.getBean(Driver)
-
-        then:
-        driver.session().run('MATCH (n) RETURN n').size() == 0
-
-        cleanup:
-        applicationContext?.stop()
-    }
+class Neo4jServerSpec extends Specification {
 
     void "test neo4j testcontainer"() {
         given:
